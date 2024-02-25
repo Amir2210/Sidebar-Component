42742b555ae35719aacd7160c070c8f9f0638fa2
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-25 17:56:58 +0200

    Add links and social brands icons to the sidebar content

--- src/cmps/Sidebar.jsx
+++ src/cmps/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { useGlobalContext } from '../AppContext'
 import logo from '../logo.svg'
+import { links } from '../data'
+import { social } from '../data'
 export function Sidebar() {
   const { isSidebarOpen, onToggleSidebar } = useGlobalContext()
   return (
@@ -8,6 +10,12 @@ export function Sidebar() {
         <img src={logo} alt="logo" />
         <button onClick={onToggleSidebar} className='close-btn'>X</button>
       </div>
+      <ul className='links'>
+        {links.map(link => <li key={link.id}><a href={link.url}>{link.icon} {link.text}</a></li>)}
+      </ul>
+      <ul className='social-links'>
+        {social.map(brand => <li key={brand.id}><a href={brand.url}>{brand.icon}</a></li>)}
+      </ul>
     </aside>
   )
 }
